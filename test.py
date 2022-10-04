546c8e756fa5e5c2c35b8b8883de7661313d8667
Chinmay Parab <chinmay.parab@softlabsgroup.com>
2022-10-04 11:32:17 +0530

    updates

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-print ("Hello word")
-print ("this is a test")
-print ("this not a test")
-
+w = float(input("what is your weight in Kgs: "))
+p = w * 2.20
+print("your weight in pounds is "+ str(p))
