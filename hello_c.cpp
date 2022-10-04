a7a253d4578c8118c912925d3ebba6bbfbda7194
Chinmay Parab <chinmay.parab@softlabsgroup.com>
2022-10-04 12:50:26 +0530

    test

---
+++ hello_c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    int a = 10;
+    int b = 15;
+    int d = (a+b);
+    
+    
+    
+    printf("%d",d);
+    return 0;
+}
--- test.py
+++ test.py
@@ -1,3 +1,31 @@
-w = float(input("what is your weight in Kgs: "))
-p = w * 2.20
-print("your weight in pounds is "+ str(p))
+import time
+
+print ("\nwelcome to Car game! \n Press enter to begin")
+engine = False
+command = input("\nEnter 'Start' to start the car. \n Enter 'Stop' to stop the car. \n Enter 'quit' to quit the game:\n")
+while command.upper()!= "QUIT":
+    if command.upper() == "START" and engine == False:
+        command = input("\nthe car is started.... \nEnter 'Stop' to stop the car\n")
+        engine = True
+        
+    elif command.upper() == "START" and engine == True:
+        command = input("\nThe engine is already on \nEnter 'Stop' to stop the car\n")
+        engine = True
+
+    elif command.upper() == "STOP" and engine == True:
+        command = input("\nthe car is Stopped.... \nEnter 'Start' to start the car\n")
+        engine = False
+    elif command.upper() == "STOP" and engine == False:
+        command = input("\nThe engine is already off.... \nEnter 'Start' to start the car\n")
+        engine = False
+    elif command.upper()!= "QUIT" and command.upper()!= "START" and command.upper()!="STOP":
+        
+        if engine == True:
+            print("\nEngline is on\n")
+        else:
+            print("\nEngine is off\n")
+        command = input("please enter valid command!\n")
+    
+else:
+    print("thanks for playing")
+    time.sleep(1)
